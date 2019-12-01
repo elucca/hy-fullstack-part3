d24ac5d062b49585dbfcbface3bab5464d2bd526
Juho Kesälä <elukka.elukka@gmail.com>
2019-12-01 15:01:01 +0200

    Exercise 3.1

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    },
+    {
+        "name": "Edsger Dijkstra",
+        "number": "01234",
+        "id": 5
+    },
+    {
+        "name": "pööp-ameeba",
+        "number": "040 238 212",
+        "id": 6
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+const port = 3001
+app.listen(port)
