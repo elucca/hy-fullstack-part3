65cc1a1f66a3e96c2952b752cc7826b77142094d
Juho Kesälä <elukka.elukka@gmail.com>
2019-12-01 16:29:31 +0200

    Exercise 3.4

--- src/index.js
+++ src/index.js
@@ -31,6 +31,11 @@ let persons = [
         "name": "pööp-ameeba",
         "number": "040 238 212",
         "id": 6
+    },
+    {
+        "name": "delete this",
+        "number": "00123",
+        "id": 7
     }
 ]
 
@@ -47,6 +52,13 @@ app.get('/api/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(204).end()
 })
 
 app.get('/info', (req, res) => {
