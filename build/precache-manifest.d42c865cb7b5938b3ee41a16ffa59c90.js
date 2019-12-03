14cc00df73b54256fd79b71d99e5227b5cf6b6e7
Juho Kesälä <elukka.elukka@gmail.com>
2019-12-03 19:35:35 +0200

    Exercise 3.9 - add front end production code

---
+++ build/precache-manifest.d42c865cb7b5938b3ee41a16ffa59c90.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c003d45dba81be376aabfa1091b6836c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58fa232f183a834bbd6d",
+    "url": "/static/js/2.dd5572f3.chunk.js"
+  },
+  {
+    "revision": "179ba235e474512c2c1d",
+    "url": "/static/js/main.58f98bcd.chunk.js"
+  },
+  {
+    "revision": "269ad4d029cce48fc3f0",
+    "url": "/static/js/runtime-main.b44955d6.js"
+  }
+]);
